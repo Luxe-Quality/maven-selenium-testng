495131490275f10caff10524e5776123aec3f8bd
AndriiCar <kravchenko.svg@gmail.com>
2024-01-07 13:42:25 +0200

    added browser.headless property

--- src/test/java/com/luxequality/helpers/CommonMethods.java
+++ src/test/java/com/luxequality/helpers/CommonMethods.java
@@ -30,7 +30,8 @@ public class CommonMethods extends PageInitializer {
 
         // set up chrome options
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless=new");
+        if (Boolean.parseBoolean(properties.getProperty("browser.headless")))
+            chromeOptions.addArguments("--headless=new");
         chromeOptions.addArguments("--start-maximized");
         chromeOptions.addArguments("--window-size=1920,1080");
 
